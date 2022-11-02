29117c67373da259c2514b31141aa19d5fa732ec
Mayurvis <96341741+Mayurvis@users.noreply.github.com>
2022-11-02 12:27:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "sre_lambda" {
-  function_name = "intern-lambda_handler"
-  role          = "arn:aws:iam::429134309155:role/lambda_dynamo_db_role"
+  function_name = "lambda_handler"
+  role          = "role arn"
   #   handler       = "lambda_function.lambda_handler"
   #   runtime       = "python3.8"
-  image_uri    = "429134309155.dkr.ecr.ap-south-1.amazonaws.com/kaisehua:v1"
+  image_uri    = ""
   package_type = "Image"
   memory_size  = "128"
   timeout      = "3"
